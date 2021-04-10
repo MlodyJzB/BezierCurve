7dd709dba64e38746d8cf5d2b791d5a74330eec8
Jakub Cimochowski <01158004@pw.edu.pl>
2021-04-10 16:03:04 +0200

    Create BezierCurve init

--- BezierCurve/BezierCurve.cpp
+++ BezierCurve/BezierCurve.cpp
@@ -1,4 +1,17 @@
 #include "BezierCurve.h"
+using namespace std;
 
-// TODO: This is an example of a library function
+BezierCurve::BezierCurve() {
+	p0 = make_tuple(0, 0);
+	p1 = make_tuple(0, 0);
+	p2 = make_tuple(0, 0);
+	p3 = make_tuple(0, 0);
+}
+
+BezierCurve::BezierCurve(std::tuple<int, int> p0, std::tuple<int, int> p1, std::tuple<int, int> p2, std::tuple<int, int> p3) {
+	this->p0 = p0;
+	this->p1 = p1;
+	this->p2 = p2;
+	this->p3 = p3;
+}
 
--- BezierCurve/BezierCurve.h
+++ BezierCurve/BezierCurve.h
@@ -1,16 +1,17 @@
 #pragma once
 #include<tuple>
-using namespace std;
 
 class BezierCurve
 {
 private:
-	tuple <int, int> p0;
-	tuple <int, int> p1;
-	tuple <int, int> p2;
-	tuple <int, int> p3;
+	std::tuple <int, int> p0;
+	std::tuple <int, int> p1;
+	std::tuple <int, int> p2;
+	std::tuple <int, int> p3;
 public:
-	BezierCurve(tuple<int, int> p0, tuple<int, int> p1, tuple<int, int> p2, tuple<int, int> p3) noexcept;
-	void setPoint(int number, tuple <int, int> new_point)
+	BezierCurve();
+	BezierCurve(std::tuple<int, int> p0, std::tuple<int, int> p1, std::tuple<int, int> p2, std::tuple<int, int> p3) noexcept;
+	std::tuple<int, int> getPoint(int number);
+	void setPoint(int number, tuple <int, int> new_point);
 };
 
